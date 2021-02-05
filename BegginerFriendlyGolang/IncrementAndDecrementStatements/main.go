ff6ffe804df735c0aa5fe9ed4fb9e43114b924b1
Justin White <jwhite1337@gmail.com>
2021-02-04 21:18:56 -0600

    added main.go to go along with lessons

---
+++ BegginerFriendlyGolang/IncrementAndDecrementStatements/main.go
@@ -0,0 +1,31 @@
+// Increment and Decrement Statements
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := 1
+	fmt.Println("a:", a)
+
+	a = a + 1
+	fmt.Println("a:", a)
+	/*
+		a += 1 // linter recommends a++ instead
+		fmt.Println("a:", a)
+		a++
+		fmt.Println("a:", a)
+	*/
+	/*
+		a--
+		fmt.Println("a:", a)
+	*/
+
+	/*
+		for i := 0; i < 11; i++ {
+			fmt.Println("i:", i)
+		}
+	*/
+
+}
